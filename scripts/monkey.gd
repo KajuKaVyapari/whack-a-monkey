5f77acc271be64e30eae0232add94d283550ed33
KajuKaVyapari <valve_1105>
2020-07-21 11:37:17 +0530

    lot of work done!!
    +made monkey scene and script
    +added hitbox to hammer and monkey
    +added random spawning for monkeys
    +added ability to kill monkeys
    
    *Todo - setup proper spawn locations for monkeys (possible_spawns
    variable in world script)

---
+++ scripts/monkey.gd
@@ -0,0 +1,28 @@
+extends Sprite
+
+
+onready var tween = $monkey_tween
+onready var hitbox_collider = $monkey_hitbox/collider
+
+var rot_deg
+var dir
+
+onready var or_pos = position
+onready var fn_pos = or_pos + 50 * dir
+
+
+func _ready():
+	
+	rotation_degrees = rot_deg
+	hitbox_collider.disabled = false
+	tween.interpolate_property(self, "position", or_pos, fn_pos, 1, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
+	tween.start()
+
+
+func _on_monkey_tween_completed(object, key):
+	
+	if object.position == fn_pos:
+		tween.interpolate_property(self, "position", fn_pos, or_pos, 1, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
+		tween.start()
+	else:
+		queue_free()
--- scripts/world.gd
+++ scripts/world.gd
@@ -1,16 +1,21 @@
 extends Node2D
 
+
 onready var monkeys = $monkeys
 onready var hammer = $hammer
 onready var hammer_tween = $hammer/tween
+onready var Monkey = load("res://scenes/monkey.tscn")
 
 
 var possible_spawns = [
-	Vector2(300, 120),
-	Vector2(100, 250)
+	make_point(Vector2(100, 250), 90, Vector2.RIGHT),
+	make_point(Vector2(250, 100), -90, Vector2.LEFT),
 ]
 
 var state = "move"
+var monkeys_list = []
+
+var i = 0
 
 
 func _ready():
@@ -24,10 +29,10 @@ func _physics_process(delta):
 	match state:
 		"move":
 			if Input.is_action_just_pressed("attack"):
+				hammer.get_node("monkey_detector/collider").disabled = false
 				state = "wait"
 				rotate_hammer()
 			hammer.global_position = get_global_mouse_position()
-		
 
 
 func rotate_hammer():
@@ -43,3 +48,23 @@ func _on_tween_completed(object, key):
 		get_viewport().warp_mouse(hammer.global_position)
 		yield(get_tree(), "idle_frame")
 		state = "move"
+		hammer.get_node("monkey_detector/collider").disabled = true
+
+
+
+func _on_monkey_detector_area_entered(area):
+	area.get_parent().queue_free()
+
+
+func make_point(spawn_point, rot_deg, dir):
+	return {"spawn_point": spawn_point, "rot_deg": rot_deg, "dir": dir}
+
+
+func _on_spawn_timer_timeout():
+	var info = possible_spawns[randi()% possible_spawns.size()]
+	monkeys_list.push_back(Monkey.instance())
+	monkeys_list[i].dir= info["dir"]
+	monkeys_list[i].rot_deg = info["rot_deg"]
+	monkeys_list[i].global_position = info["spawn_point"]
+	monkeys.add_child(monkeys_list[i])
+	i += 1
