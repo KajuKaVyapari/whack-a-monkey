f3b917f8a86422adbbe027ad6a14c318c3e006c1
Rehan Daphedar <kajukavyapari@pop-os.localdomain>
2020-07-21 08:58:05 +0530

    setup hammer
    +added state machine
    +added basic tween (hammer rotates when you click)
    +added follow mouse for hammer
    +made mouse cursor invisible, and made it so that the mouse cursor warps
    back to the hammer when roatt

--- scripts/world.gd
+++ scripts/world.gd
@@ -10,9 +10,36 @@ var possible_spawns = [
 	Vector2(100, 250)
 ]
 
+var state = "move"
+
 
 func _ready():
 	
 	randomize()
-	hammer_tween.interpolate_property(hammer, "global_position",  hammer.global_position, possible_spawns[randi() % 2], 1, hammer_tween.TRANS_SINE, hammer_tween.EASE_IN_OUT)
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+
+func _physics_process(delta):
+	
+	match state:
+		"move":
+			if Input.is_action_just_pressed("attack"):
+				state = "wait"
+				rotate_hammer()
+			hammer.global_position = get_global_mouse_position()
+		
+
+
+func rotate_hammer():
+	hammer_tween.interpolate_property(hammer, "rotation_degrees", 0, -45, .5, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
 	hammer_tween.start()
+
+
+func _on_tween_completed(object, key):
+	if object.rotation_degrees == -45:
+		hammer_tween.interpolate_property(hammer, "rotation_degrees", -45, 0, .5, Tween.TRANS_ELASTIC, Tween.EASE_IN_OUT)
+		hammer_tween.start()
+	else:
+		get_viewport().warp_mouse(hammer.global_position)
+		yield(get_tree(), "idle_frame")
+		state = "move"
