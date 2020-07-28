8841ee94b20d1d77f5e0a0c95aaa4442c655377f
KajuKaVyapari <rehan.daphedar@gmail.com>
2020-07-28 12:24:31 +0530

    made HUD, started main menu, made pause button + textures and also UI script

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,6 +1,18 @@
 extends Control
 
 
+var paused = false
 
 func _on_exit_button_pressed():
 	get_tree().quit()
+
+
+func _on_pause_button_pressed():
+	if not paused:
+		$buttons/pause_button.texture_normal = load("res://assets/ui/play_button.png")
+		paused = true
+		get_tree().paused = true
+	else:
+		$buttons/pause_button.texture_normal = load("res://assets/ui/pause_button.png")
+		paused = false
+		get_tree().paused = false
