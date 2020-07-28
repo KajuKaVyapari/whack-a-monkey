dfb1e0186f20e1069066d12a66068a3fe519f841
KajuKaVyapari <rehan.daphedar@gmail.com>
2020-07-28 21:09:35 +0530

    Main Menu Stuff
    +finished ingame hud
    +finished main menu
    *TODO* add a loss screen/ make losing scene

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -9,10 +9,30 @@ func _on_exit_button_pressed():
 
 func _on_pause_button_pressed():
 	if not paused:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		$buttons/pause_button.texture_normal = load("res://assets/ui/play_button.png")
 		paused = true
 		get_tree().paused = true
 	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 		$buttons/pause_button.texture_normal = load("res://assets/ui/pause_button.png")
 		paused = false
 		get_tree().paused = false
+
+
+func _on_menu_button_pressed():
+	paused = false
+	get_tree().paused = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	get_tree().change_scene("res://scenes/main_menu.tscn")
+
+
+func _on_play_button_pressed():
+	paused = false
+	get_tree().paused = false
+	get_tree().change_scene("res://scenes/world.tscn")
+
+
+func _ready():
+	if not $play_button == null:
+		$play_button.texture_normal = load("res://assets/ui/play_button.png")
